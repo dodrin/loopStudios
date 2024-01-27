f6f9c4eeddc11df2ca3fa1abeadc436643a9447d
Rina Inada <indrn@icloud.com>
2024-01-26 17:48:23 -0800

    Move Header into a new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-
-// SVG assets
 import Logo from "./assets/logo.svg?react";
-
-import Burger from "./assets/icon-hamburger.svg?react";
 import interactive from "./assets/mobile/image-interactive.jpg";
-import mobileHero from "./assets/mobile/image-hero.jpg";
+
 
 import Attribution from "./components/Attribution";
 import { useRef } from "react";
+import { Header } from "./components/Header";
 import Footer from "./components/footer";
 
 function App() {
@@ -25,41 +22,8 @@ function App() {
 
   return (
     <>
-      {/* bg image section */}
-      <div
-        className="p-5 py-8"
-        style={{
-          backgroundImage: `url(${mobileHero})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
-        {/* Top nav menu */}
-        <div className="flex justify-between items-center">
-          <a href="#">
-            <Logo alt="Loopstudios-logo" />
-          </a>
-          <div
-            className="flex flex-col fixed top-0 left-0 w-screen bg-black overflow-hidden hidden"
-            ref={menuRef}
-          >
-            {menu.map((menuItem) => (
-              <a className="text-white text-3xl" key={menuItem}>
-                {menuItem.toUpperCase()}
-              </a>
-            ))}
-          </div>
-          <div className="buger" onClick={toggleMenu}>
-            <Burger alt="Menu-icon" />
-          </div>
-        </div>
-        <div className="hero">
-          <h1 className="text-white text-5xl border-2 border-white my-52 p-5 text-left">
-            {`Immersive experiences that deliver`.toUpperCase()}
-          </h1>
-        </div>
-      </div>
-      {/* bg image section end */}
+      <Header toggleMenu={toggleMenu} Logo={Logo} menuRef={menuRef} menu={menu} />
+      
       {/* The leader in interractive VR section */}
       <div className="flex flex-col m-6 my-12">
         <img
